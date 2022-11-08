7652d5273bd6f1d44c62329c37541ea23ad8f6e2
Allisson Bezerra <allisson.geografia@gmail.com>
2022-11-08 08:43:56 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -10,14 +10,27 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private int turn;
+	private Collor currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Collor.WHITE;
 		initialSetup();
-		
 	}
 	
+		
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Collor getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for(int i = 0; i<board.getRows(); i++) {
@@ -43,6 +56,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -58,6 +72,10 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException(" There is no piece on source position");
 		}
+		if(currentPlayer !=((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
+		
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
@@ -70,6 +88,11 @@ public class ChessMatch {
 		}
 	}
 	
+	
+	private void nextTurn() {
+		turn ++;
+		currentPlayer = (currentPlayer == Collor.WHITE) ? Collor.BLACK : Collor.WHITE;
+	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
